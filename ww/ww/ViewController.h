8d902d901d5a9b0e68aa51f65b627534e7d19578
MS <1604818152@qq.com>
2015-12-04 19:38:18 +0800

    这是李海星

---
+++ ww/ww/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ww
+//
+//  Created by MS on 15-12-4.
+//  Copyright (c) 2015年 王中刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ww/ww/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ww
+//
+//  Created by MS on 15-12-4.
+//  Copyright (c) 2015年 王中刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
